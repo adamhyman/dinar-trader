2ff6f42dda608972c0ea0fe8264b03dffee2ab42
Adam Hyman <adamhyman@gmail.com>
2017-07-13 23:08:51 -0700

    Added GDAX

--- exchange_query.py
+++ exchange_query.py
@@ -24,7 +24,7 @@ sleep_time_sec = 15
 # Rand Uni [0, .1] will be added to this.  
 # This makes tying a pair of buy-and-sell orders together easy
 # because we can now tie on quantiy.
-eth_trade_base = .4
+eth_trade_base = .3
 
 # Key locations
 kraken_key = '../kraken.key'
@@ -47,7 +47,7 @@ root.addHandler(ch)
 # Session initialization
 kraken = exchange_session(exchange='kraken', path_to_key=kraken_key)
 gemini = exchange_session(exchange='gemini', path_to_key=gemini_key)
-gdax = exchange_session(exchange='gdax', path_to_key=gemini_key)
+gdax = exchange_session(exchange='gdax', path_to_key=gdax_key)
 
 # Set up CSV log file and write headers
 logfile = open("log.csv", 'w', newline='')
@@ -58,6 +58,7 @@ log_writer.writerow(['Date', 'Time', 'Kraken Balance (USD)', 'Kraken Balance (ET
 logging.info ("Getting balances.")
 kbalances = kraken.get_balances()
 gbalances = gemini.get_balances()
+gdaxbalances = gdax.get_balances()
 logging.info ("")
 
 logging.info ("Running exchange queries and looking for opportunities.")
@@ -67,22 +68,29 @@ logging.info ("Running exchange queries and looking for opportunities.")
 while True:
     try:
 
-        eth_trade_qty = str(round((eth_trade_base + random.randint(1, 100)/1000), 3))
-        print (eth_trade_qty)
+        eth_trade_qty = str(round((eth_trade_base + random.randint(1, 100)/10000), 3))
+        # print (eth_trade_qty)
 
         # Get the bid and asking prices
         logging.info ("Getting prices.")
         k_trade_info = kraken.get_trade_info("ETHUSD")
         k_ask_eth = k_trade_info["ask"] * 1.0026
         k_bid_eth = k_trade_info["bid"] * 0.9974
+        
         g_trade_info = gemini.get_trade_info("ETHUSD")
         g_ask_eth = g_trade_info["ask"] * 1.0025
         g_bid_eth = g_trade_info["bid"] * 0.9975
+
+        gdax_trade_info = gdax.get_trade_info("ETHUSD")
+        gdax_ask_eth = gdax_trade_info["ask"] * 1.0026
+        gdax_bid_eth = gdax_trade_info["bid"] * 1.9974
 
         logging.info ("Kraken Bid:  %s" % k_bid_eth)
         logging.info ("Kraken Ask:  %s" % k_ask_eth)
         logging.info ("Gemini Bid:  %s" % g_bid_eth)
         logging.info ("Gemini Ask:  %s" % g_ask_eth)
+        logging.info ("Gdax Bid:  %s" % gdax_bid_eth)
+        logging.info ("Gdax Ask:  %s" % gdax_ask_eth)
         logging.info ("")
 
         log_writer.writerow([datetime.now().strftime('%Y-%m-%d'), datetime.now().strftime('%H:%M:%S.%f'), str(kbalances["USD"]), str(kbalances["ETH"]), str(kbalances["BTC"]), str(gbalances["USD"]), str(gbalances["ETH"]), str(gbalances["BTC"]), str(k_ask_eth), str(k_bid_eth), str(g_ask_eth), str(g_bid_eth)])
@@ -102,6 +110,7 @@ while True:
             logging.info ("Transactions Complete")
             kbalances = kraken.get_balances()
             gbalances = gemini.get_balances()
+            gdaxbalances = gdax.get_balances()
 
         # Buy Kraken, Sell Gemini
         if float(g_bid_eth) > float(k_ask_eth) and kbalances["USD"] > float(200) and gbalances["ETH"] > float(1):
@@ -118,6 +127,8 @@ while True:
             logging.info ("Transactions Complete")
             kbalances = kraken.get_balances()
             gbalances = gemini.get_balances()
+            gbalances = gemini.get_balances()
+            gdaxbalances = gdax.get_balances()
             # Skip wait statement and restart loop as an
             # arbitrage opportunity may exist.
             continue 
