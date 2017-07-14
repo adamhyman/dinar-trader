ae726263ccd8d7b2fb7e4e995f891f94f305e207
Adam Hyman <adamhyman@gmail.com>
2017-07-13 23:09:14 -0700

    Added GDAX

--- exchange_session.py
+++ exchange_session.py
@@ -33,6 +33,15 @@ class exchange_session(object):
                 secret = f.readline().strip()
                 self.session = GeminiSession(key, secret, False)
             logging.info ("Gemini session configured.")
+        # Create a GDAX exchange session object
+        elif (exchange.lower() == "gdax"):
+            with open(path_to_key, 'r') as f:
+                key = f.readline().strip()
+                secret = f.readline().strip()
+                passphrase = f.readline().strip()
+                print ('Key ' + key)
+            self.session = gdax.AuthenticatedClient(key, secret, passphrase)
+            logging.info ("GDAX session configured.")
         elif (exchange == ""):
             raise ValueError("Missing exchange name.")
             
@@ -43,6 +52,8 @@ class exchange_session(object):
                 return "XETHZUSD"
             if (self.exchange.lower() == "gemini"):
                 return "ethusd"
+            if (self.exchange.lower() == "gdax"):
+                return "ETH-USD"
 
     def get_balances(self):
         ## Returns the balance of account in a dict format
@@ -70,6 +81,19 @@ class exchange_session(object):
                 logging.info ("Gemini ETH: %s" % balance[2]["available"])
                 logging.info ("Gemini BTC: %s" % balance[0]["available"])
             return {'USD':float(balance[1]["available"]), 'ETH':float(balance[2]["available"]), 'BTC':float(balance[0]["available"])}
+        elif (self.exchange.lower() == "gdax"):
+            balance = self.get_accounts()
+            self.USD = balance[0]['balance']
+            self.LTC = balance[1]['balance']
+            self.ETH = balance[2]['balance']
+            self.BTC = balance[3]['balance']
+            if self.debug:
+                logging.info ("GDAX USD:  %s" % balance[0]['balance'])
+                logging.info ("GDAX ETH:  %s" % balance[2]['balance'])
+                logging.info ("GDAX BTC:  %s" % balance[3]['balance'])
+            return {'USD':float(balance[0]['balance']), 'ETH':float(balance[2]['balance']), 'BTC':float(balance[3]['balance'])}
+            
+
             
     def get_trade_info(self, ticker_pair="ETHUSD"):
         ## Returns recent trading activity for a symbol
@@ -93,24 +117,36 @@ class exchange_session(object):
             if (ticker_pair == "ETHUSD"):
                 gbalance = self.session.get_ticker(self.get_pair_name("ETHUSD"))
                 return {'ask':float(gbalance["ask"]), 'bid':float(gbalance["bid"])}
+        
+        elif (self.exchange.lower() == "gdax"):
+            if (ticker_pair == "ETHUSD"):
+                gdax_balance = self.session.get_product_ticker(self.get_pair_name("ETHUSD"))
+                return {'ask':float(gdax_balance['ask']), 'bid':float(gdax_balance['bid'])}
+        
             
     def get_balance_usd(self):
         if (self.exchange.lower() == "kraken"):
             return float(self.session.query_private('Balance')['result']["ZUSD"])
         elif (self.exchange.lower() == "gemini"):
             return float(self.session.get_balances()[1]["available"])
+        elif (self.exchange.lower() == "gdax"):
+            return float(self.session.get_accounts()[0]['balance'])
             
     def get_balance_eth(self):
         if (self.exchange.lower() == "kraken"):
             return float(self.session.query_private('Balance')['result']["XETH"])
         elif (self.exchange.lower() == "gemini"):
             return float(self.session.get_balances()[2]["available"])
+        elif (self.exchange.lower() == "gdax"):
+            return float(self.session.get_accounts()[2]['balance'])
             
     def get_balance_btc(self):
         if (self.exchange.lower() == "kraken"):
             return float(self.session.query_private('Balance')['result']["XXBT"])
         elif (self.exchange.lower() == "gemini"):
             return float(self.session.get_balances()[0]["available"])
+        elif (self.exchange.lower() == "gdax"):
+            return float(self.session.get_accounts()[3]['balance'])
 
     def get_exchange_type(self):
         return self.exchange
